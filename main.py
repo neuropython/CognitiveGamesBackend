c829bd8eeb923f378ac86deda26caf85f4924e4c
Damian <damianpietron007@gmail.com>
2024-05-19 16:20:15 +0200

    Implement adding and deleting user

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field, EmailStr
 from typing import List, Optional, Union
 from enum import Enum
-app = FastAPI()
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+from bson import ObjectId
+from fastapi.exceptions import HTTPException
+from urllib.parse import quote_plus
 
+app = FastAPI(debug=True)
+load_dotenv()
+_password = quote_plus(str(os.getenv("MONGO_PASSWORD")))
+_username = quote_plus(str(os.getenv("MONGO_USERNAME")))
+MONGO_URI = f"mongodb+srv://{_username}:{_password}@cognitivedatabase.ivg4g6i.mongodb.net/?retryWrites=true&w=majority&appName=CognitiveDatabase"
 ## Define database_models:
 
 class GameTypes(Enum):
@@ -31,21 +41,35 @@ class UserGames(BaseModel):
     score: int
     date: str
 
-### Add mock data to test:
-mock_user = User(id=1, first_name="John", last_name="Doe", email="example@gmail.com", password="example", username="example")
-mock_game = Games(id=1, game_type=GameTypes.color_game)
-mock_user_game = UserGames(id=1, user_id=1, game_id=1, score=100, date="2021-09-01")
- 
+### Connect to MongoDB:
+client = MongoClient(MONGO_URI)
+db = client["Data"]
+
+users_collection = db["Users"]
+games_collection = db["Games"]
+user_games_collection = db["UserGames"]
+
+try:
+    client.admin.command('ismaster')
+    print("Connected to MongoDB")
+except Exception as e:
+    print("Cannot connect to MongoDB: ", e)
+
 ### Define API endpoints:
 @app.get("/")
 def read_root():
     return {"CogniBackendApp"}
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int) -> User:
-    return {"user_id": user_id}
+@app.get("/users/{user_id}", response_model=User)
+def read_user(user_id: int) -> Union[User, HTTPException]:
+    user = users_collection.find_one({"id": user_id})
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 @app.get("/users")
+def read_users() -> List[User]:
+    return {"users": "all"}
 
 @app.get("/users/{user_id}/games")
 def read_user_games(user_id: int) -> List[UserGames]:
@@ -55,3 +79,14 @@ def read_user_games(user_id: int) -> List[UserGames]:
 def read_user_game(user_id: int, game_id: int) -> UserGames:
     return {"user_id": user_id, "game_id": game_id}
 
+@app.post("/users")
+def create_user(user: User) -> User:
+    id = user.id
+    check_user_exists(id)
+    users_collection.insert_one(user.dict())
+    return user
+
+def check_user_exists(id: str):
+    user = users_collection.find_one({"_id": id})
+    if user is not None:
+        raise HTTPException(status_code=400, detail="User already exists")
--- test_server.py
+++ test_server.py
@@ -2,3 +2,14 @@ import requests
 
 # Test the server
 print(requests.get('http://127.0.0.1:8000').text)
+
+print(requests.get('http://127.0.0.1:8000/users/1').text)
+
+response = requests.post("http://127.0.0.1:8000/users", json={
+    "id": 2,
+    "first_name": "John",
+    "last_name": "Doe",
+    "email": "johndoe@example.com",
+    "password": "secret",
+    "username": "johndoe"
+})
