0d38fd27ed0f169d6a8963d88701b1e54c646381
Damian <damianpietron007@gmail.com>
2024-05-23 21:17:20 +0200

    chore: Update test_server.py with user-related API calls and remove unnecessary code

--- test_server.py
+++ test_server.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 # Test the server
 headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNzE2NDk0MzI2fQ.5Vt2pL8lG9AuyLpogj9Dhyi0bjMvM2jgIbIwsdQSDyY"}
-refresh_token_header = {"refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNzE5MDY2MTM2fQ.5d_eMPiimrpWmGyMLC_CSy-qx-NO5NBrTFHpOoAGINc"}
+refresh_token = {"refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNzE5MDY2MTM2fQ.5d_eMPiimrpWmGyMLC_CSy-qx-NO5NBrTFHpOoAGINc"}
 ### Test the server
 print(requests.get('http://127.0.0.1:8000', headers=headers).text)
 
-#################### USER ######################3
+#################### USER ######################
 
 # get single user
-print(requests.get('http://127.0.0.1:8000/users/1', headers=headers).text)
+# print(requests.get('http://127.0.0.1:8000/users/1', headers=headers).text)
 
 # get all users
 # print(requests.get('http://127.0.0.1:8000/users'))
@@ -28,18 +28,11 @@ print(requests.get('http://127.0.0.1:8000/users/1', headers=headers).text)
 ### Test game endpoints ###
 
 # get all game scores related to a user
-# response = requests.get("http://127.0.0.1:8000/users/games/2",  headers=headers)
+# response = requests.get("http://127.0.0.1:8000/users/games/1",  headers=headers)
 # print(response.text)
 
 # get all games
 # response = requests.get("http://127.0.0.1:8000/games",  headers=headers)
-# print(response.text)
-
-# create game - for backend ####### BACKEND
-# response = requests.post('http://127.0.0.1:8000/games', json={
-#         "id": 1,
-#         "game_type": "memory_game"
-#         })
 # print(response.text)
 
 # print(requests.get('http://127.0.0.1:8000/games').text)
@@ -53,7 +46,7 @@ print(requests.get('http://127.0.0.1:8000/users/1', headers=headers).text)
 # response_me = requests.get('http://127.0.0.1:8000/me', headers=headers)
 # print(response_me.text)
 
-# login = requests.post('http://127.0.0.1:8000/refresh_token', params=refresh_token_header)
+# login = requests.post('http://127.0.0.1:8000/refresh_token', params=refresh_token)
 # print(login.text)  
 
 # response = requests.get('http://127.0.0.1:8000/all_scores/1')
